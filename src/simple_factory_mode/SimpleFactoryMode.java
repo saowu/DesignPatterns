58958971cdb1af108bfafa503016a00f50304082
wuyanbo <786021717@qq.com>
2019-04-08 14:28:22 +0800

    简单工厂模式

--- src/simple_factory_mode/SimpleFactoryMode.java
+++ src/simple_factory_mode/SimpleFactoryMode.java
@@ -1,8 +1,9 @@
 package simple_factory_mode;
 
+import java.util.Scanner;
+
 /**
  * @description: 简单工厂模式实现计算器
- *
  * @author: wuyanbo
  * @create: 2019-04-08 12:07
  **/
@@ -10,5 +11,17 @@ package simple_factory_mode;
 public class SimpleFactoryMode {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        char c = scanner.next().charAt(0);
+        double b = scanner.nextDouble();
+
+        Operation operation = OperationFactory.createOperation(c);
+        operation.setNumberA(a);
+        operation.setNumberB(b);
+
+        System.out.println(operation.getResult());
+
     }
+
 }
