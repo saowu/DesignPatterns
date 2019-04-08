c73b1fa0c87559673c34ed3fb2ae8b96e83b92c6
wuyanbo <786021717@qq.com>
2019-04-08 12:15:07 +0800

    Commit测试

---
+++ src/simple_factory_mode/SimpleFactoryMode.java
@@ -0,0 +1,14 @@
+package simple_factory_mode;
+
+/**
+ * @description: 简单工厂模式实现计算器
+ *
+ * @author: wuyanbo
+ * @create: 2019-04-08 12:07
+ **/
+
+public class SimpleFactoryMode {
+    public static void main(String[] args) {
+
+    }
+}
