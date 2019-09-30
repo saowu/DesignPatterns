2ca670fa6f2190b6ceb1376eb424f8ecc7c8b30b
wuyanbo <786021717@qq.com>
2019-09-30 19:57:13 +0800

    适配器模式

---
+++ src/adapter_mode/Adaptee.java
@@ -0,0 +1,15 @@
+package adapter_mode;
+
+/**
+ * @description: 需要适配的类
+ * @author: wuyanbo
+ * @create: 2019-09-30 19:56
+ **/
+
+public class Adaptee {
+
+    public void spcificRequest() {
+        System.out.println("特殊请求！");
+    }
+
+}
